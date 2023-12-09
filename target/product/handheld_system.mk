4e902a560e9a302ad7a8af467061cbc333f6e6db
Tommaso Fonda <fondatommaso2@gmail.com>
2023-12-09 10:32:45 +0100

    Build F-DroidPrivilegedExtension
    Change-Id: I91ae929c42564407d5d9e42f2940b17aea14f042

--- target/product/handheld_system.mk
+++ target/product/handheld_system.mk
@@ -48,6 +48,7 @@ PRODUCT_PACKAGES += \
     DownloadProviderUi \
     EasterEgg \
     ExternalStorageProvider \
+    F-DroidPrivilegedExtension \
     FusedLocation \
     InputDevices \
     KeyChain \
