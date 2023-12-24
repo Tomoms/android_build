b18876019871db418906b6baf1e5e4889359d784
Tommaso Fonda <fondatommaso2@gmail.com>
2023-12-24 10:20:31 +0100

    envsetup: No ccache compression, please
    Change-Id: I5fc1ce63d83790e8c692966bfd5872976ccfcc32

--- envsetup.sh
+++ envsetup.sh
@@ -2125,7 +2125,6 @@ if [ -z ${CCACHE_EXEC} ]; then
         if [ -z ${CCACHE_DIR} ]; then
             export CCACHE_DIR=${HOME}/.ccache
         fi
-        $ccache_path -o compression=true
         echo -e "ccache enabled and CCACHE_EXEC has been set to : $ccache_path"
     else
         echo -e "ccache not found installed!"
