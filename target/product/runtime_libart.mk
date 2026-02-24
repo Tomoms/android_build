e013695255d10e943610f979613afe5db0007a8c
Tommaso Fonda <fondatommaso2@gmail.com>
2026-02-24 10:46:37 +0100

    tweak dexopt
    Change-Id: I6c3270776b333068774e2b31e5aa85845b1d592c

--- target/product/runtime_libart.mk
+++ target/product/runtime_libart.mk
@@ -120,9 +120,9 @@ PRODUCT_SYSTEM_PROPERTIES += \
     pm.dexopt.install-bulk-secondary?=verify \
     pm.dexopt.install-bulk-downgraded?=verify \
     pm.dexopt.install-bulk-secondary-downgraded?=verify \
-    pm.dexopt.bg-dexopt?=speed-profile \
+    pm.dexopt.bg-dexopt?=speed \
     pm.dexopt.ab-ota?=speed-profile \
-    pm.dexopt.inactive?=verify \
+    pm.dexopt.inactive?=speed-profile \
     pm.dexopt.cmdline?=verify \
     pm.dexopt.shared?=speed
 
